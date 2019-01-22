1868e4affdb807158b2cc30c42fcdcb2bdba4aa7
Christopher Lindsay <42160806+clindsa1@users.noreply.github.com>
2019-01-22 10:40:21 -0500

    single function for opening data file

--- Sequence_Recorder.ino
+++ Sequence_Recorder.ino
@@ -1,4 +1,5 @@
 //creates recording files from F1 to F8
+//files Sequence_Recordersaved to SD card
 //plays back recorded files
 
 #include <SPI.h>
@@ -8,6 +9,7 @@ const int chipSelect = 4;
 
 char f_name_which; bool recording; long start;
 File dataFile;
+int mode; //0=stoped, 1=keyboard, 2=playing, 3=recording
 
 void setup() {
   //50ms between characters = 200cps -> the kps from the host
@@ -19,74 +21,81 @@ void setup() {
     Serial.println("Card failed, or not present");
     return;
   }
-  Serial.println("card initialized.");
-  f_name_which = 0; recording = false;
+  Serial.println("Card initialized.");
+  f_name_which = '0'; recording = false;
+  mode = 0;
 }
 
 void loop() {
   while (Serial.available() == 0) { ;} //wait for character
-  long next = millis(); char b = (char)Serial.read();
+  long next = millis(); char input = (char)Serial.read();
 
-  if (b == 0x1B) { //function key
-    FNumCheck();
+  if (input == 0x1B) { //function key
+    FNum_Press_Check();
     while (Serial.available() == 0) { ;} //wait for character
-    next = millis(); b = (char)Serial.read(); //get next character after F#
+    long next = millis(); input = (char)Serial.read(); //get next character after F#
   }
 
-  if (b == '*') { //player mode
+  if (input == '*') { //player mode
     Serial.println("Player Mode");
     while (Serial.available() == 0) { ;} //wait for character
-    b = (char)Serial.read(); //get next character after F#
-    FNum_Player(b);
+    input = (char)Serial.read(); //get next character, file to play
+    FNum_Player(input);
   }
 
+  /* '1' is stop */
+
+  /* '0' is start */
+
   if (recording) {
-    next = millis();
     long delta = next - start;
     Serial.print("delay: "); Serial.println(delta);
     start = next;
 
     //create log file line
-    String num = String(delta, DEC); String c = String(b);
-    String val = "'" + c + "'," + num;
+    String num = String(delta, DEC); String c = String(input);
+    String val = "'" + c + "'," + num; //the quotes make the file CSV, but are not needed for us
 
-    FNum_Write_Opener(); //open correct file
+    FNum_File_Opener(false, f_name_which); //open correct file
   
-    dataFile.println(val); Serial.println(val); //save, puts the /r/n into text
+    dataFile.println(val); Serial.println(val); //save, puts the \r\n into text
     
     dataFile.close(); //have to close the file to ensure commit data
+
+    //start = next; //may want to time the file open-write-close block
   }
 }
 
-void FNum_Write_Opener () {
-  switch (f_name_which) { //open correct file
+void FNum_File_Opener (bool RW, char file) {
+  int fmode = (RW ? FILE_READ : FILE_WRITE);
+  switch (file) { //open correct file
     case '1':
-      dataFile = SD.open("ONE.TXT", FILE_WRITE); break; //files always seem to b ucase
+      dataFile = SD.open("ONE.TXT", fmode); break; //files always seem to b ucase
     case '2':
-      dataFile = SD.open("TWO.TXT", FILE_WRITE); break;
+      dataFile = SD.open("TWO.TXT", fmode); break;
     case '3':
-      dataFile = SD.open("THREE.TXT", FILE_WRITE); break;
+      dataFile = SD.open("THREE.TXT", fmode); break;
     case '4':
-      dataFile = SD.open("FOUR.TXT", FILE_WRITE); break;
+      dataFile = SD.open("FOUR.TXT", fmode); break;
     case '5':
-      dataFile = SD.open("FIVE.TXT", FILE_WRITE); break;
+      dataFile = SD.open("FIVE.TXT", fmode); break;
     case '6':
-      dataFile = SD.open("SIX.TXT", FILE_WRITE); break;
+      dataFile = SD.open("SIX.TXT", fmode); break;
     case '7':
-      dataFile = SD.open("SEVEN.TXT", FILE_WRITE); break;
+      dataFile = SD.open("SEVEN.TXT", fmode); break;
     case '8':
-      dataFile = SD.open("EIGHT.TXT", FILE_WRITE); break;
+      dataFile = SD.open("EIGHT.TXT", fmode); break;
     default:
       return; break;
   }
 
   if (!dataFile) { //something has gone very wrong
-    Serial.println("error opening datalog.txt");
+    Serial.println("error opening datalog");
     while (true) { ;} //infinte loop
   }
 }
 
-void FNumCheck () { //function key format 0x1B, [, 1, F1 = 1 F2 = 2..., ~
+void FNum_Press_Check () { //function key format 0x1B, [, 1, F1 = 1 F2 = 2..., ~
   //the 0x1B was parsed by calling function
   while (Serial.available() == 0) { ;} //wait for character
   char s = (char)Serial.read(); //[
@@ -111,27 +120,9 @@ void FNumCheck () { //function key format 0x1B, [, 1, F1 = 1 F2 = 2..., ~
 }
 
 void FNum_Player (char f_name) {
-  switch (f_name) { //open correct file
-    case '1':
-      dataFile = SD.open("ONE.TXT", FILE_READ); break;
-    case '2':
-      dataFile = SD.open("TWO.TXT", FILE_READ); break;
-    case '3':
-      dataFile = SD.open("THREE.TXT", FILE_READ); break;
-    case '4':
-      dataFile = SD.open("FOUR.TXT", FILE_READ); break;
-    case '5':
-      dataFile = SD.open("FIVE.TXT", FILE_READ); break;
-    case '6':
-      dataFile = SD.open("SIX.TXT", FILE_READ); break;
-    case '7':
-      dataFile = SD.open("SEVEN.TXT", FILE_READ); break;
-    case '8':
-      dataFile = SD.open("EIGHT.TXT", FILE_READ); break;
-    default:
-      Serial.println(" play file not found");
-      return; break;
-  }
+  FNum_File_Opener(true, f_name);
+
+  Serial.println("Playing File");
   
   while (dataFile.available()) { //loop through file
     String line = "";
@@ -140,19 +131,17 @@ void FNum_Player (char f_name) {
       in = dataFile.read();
       line = line + String(in);
     }
-    
+
+    //it just seemed to be easier to create a line string then parse it
     int len = line.length();
     char note = line.charAt(1);
     String duration = line.substring(4);
     int dur = duration.toInt();
 
-    //call player funciton here
-    Serial.print("line: ");
-    Serial.println(line); //test found line of text
-    Serial.print("note: ");
-    Serial.println(note);
-    Serial.print("Dur: ");
-    Serial.println(dur);
+    /* call player funciton here */
+    Serial.print("line: "); Serial.println(line); //test found line of text
+    Serial.print("note: "); Serial.println(note); 
+    Serial.print("duration: ");  Serial.println(dur);
     
     long start = millis();
     if (dur <= 0) { break; }
